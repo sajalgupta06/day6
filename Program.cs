f1d94dd625e828ddc0aacf5c796d911e037a9f7a
Sajal Gupta <guptasajal6@gmail.com>
2023-07-17 16:46:49 +0530

    first Commit

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleAppExercise2
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter name , email, password to create account");
+            Console.WriteLine("Enter Name , Email, Password to create account");
             try
             {
                 string name = Console.ReadLine();
@@ -29,19 +29,36 @@ namespace ConsoleAppExercise2
                 {
                     throw new OurExc("Name nust be 6 characters long");
                 }
+
+                if (email.Length < 1)
+                {
+                    throw new OurExc("Email cannot be blank");
+                }
                 else if (password.Length < 6)
                 {
                     throw new OurExc("Name nust be 6 characters long");
 
                 }
+
+                else
+                {
+                    Console.WriteLine("Account created Successfully");
+                }
             }
             catch (OurExc ex)
             {
+                Console.WriteLine("Validation Error: "+ ex.Message);
+            }
+
+            catch (FormatException ex)
+            {
                 Console.WriteLine(ex.Message);
+
             }
 
             catch (Exception ex)
             {
+                Console.WriteLine("Error"+ ex.Message);
 
             }
 
